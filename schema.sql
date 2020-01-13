e290092aedaf2752f1f8b96ae9875f2760ea4c98
David Bolton <smdeox@live.com>
2020-01-13 10:04:58 -0500

    Added schema/seed

---
+++ schema.sql
@@ -0,0 +1,38 @@
+-- What do we want to know about owners?
+create table owners (
+    id serial primary key,
+    name text,
+    phone_number varchar(20)
+
+    -- Do owners have one pet or many
+    -- pet_id integer reference pets (id); -- :()
+
+);
+
+
+
+-- What do we want to know about pets?
+create table pets (
+    id serial primary key,
+    name text,
+    species varchar(100),
+
+     -- we can derive the age from a birthdate
+     birthdate date, 
+     -- When you have the info in another table, ask yourself:
+     -- Do pets have one owner?
+     -- Do pets have many owners?
+     -- If pets have one and only one
+     -- Then you put the foreign key right here
+     owner_id integer REFERENCES owners (id)
+     -- Th integer references the id in the table "owners"
+);
+
+
+
+-- Chris will demo many-to-many
+-- create table pets_owners (
+--     -- This table needs no id
+--     owner_id integer references owners (id),
+--     pet_id integer REFERENCES pets (id)
+-- );
---
+++ seed.sql
@@ -0,0 +1,14 @@
+insert into owners
+    (name, phone_number)
+    VALUES
+    ('chris', '8675309'),
+    ('aylor', '5554321');
+
+insert into pets 
+    (name, species, birthdate, owner_id)
+VALUES
+    ('oakley', 'cat', '2010-05-30', 1),
+    ('milla', 'tortoise', '2005-02-28', 1),
+    ('dexter', 'dog', '2008-02-20', 2),
+     ('hank', 'dog', '2010-03-20', 2),
+      ('seymour', 'cat', '1902-01-04', 2);
