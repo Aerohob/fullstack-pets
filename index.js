849d324c6f9d763c9787265562f02d2cac9c11c6
David Bolton <smdeox@live.com>
2020-01-13 13:56:50 -0500

    Added CRUD functions

--- index.js
+++ index.js
@@ -5,8 +5,30 @@ const pets = require('./pets')
 
 // });
 
+/* For all pets
 async function main() {
     const thePets = await pets.all();
     console.log(thePets);
 }
 main();
+*/
+
+/* For one
+async function main() {
+    const aPet = await pets.one(1);
+    console.log(aPet);
+}
+main();
+*/
+/* To delete
+async function main() {
+    const result = await pets.del(1);
+    console.log(result)
+}
+main();
+*/
+async function main() {
+    const result = await pets.updateName(6, 'Does it blend');
+    console.log(result)
+}
+main()
--- pets.js
+++ pets.js
@@ -7,27 +7,45 @@ function create() {
 }
 
 // Retrieve
-function one() {
-
+async function one(id) {
+    try {
+        //let onePet = await db.one(`select * from pets where id = ${id}`)
+        //console.log(onePet);
+        let onePet = await db.one(`select * from pets where id=$1`, [id])
+        return onePet;
+    } catch (err) {
+        return [];
+    }
 }
+// Promise version
+/*
+function one(id) {
+    return db.one(`select * from pets where id=${id}`)
+    .catch(err => {
+        console.log(err);
+        return null
+    })
+}
+*/
 
-// async function all() {
-//     const allPets = await db.query(`select * from pets`)
-//     .then(data => {
-//         console.log(data);
-//         return data;
-//     }).catch(err => {
-//         console.log(err);
-//         return [];
-//     })
-//     console.log(allPets)
-//     return allPets;
-// }
+// ALL VERSION
+async function all() {
+    const allPets = await db.query(`select * from pets`)
+        .then(data => {
+            console.log(data);
+            return data;
+        }).catch(err => {
+            console.log(err);
+            return [];
+        })
+    console.log(allPets)
+    return allPets;
+}
 
 
 async function all() {
     try {
-        const thePets = db.query(`select * from pets`)
+        const thePets = await db.query(`select * from pets`)
         console.log(thePets);
         return thePets;
     } catch (err) {
@@ -36,20 +54,41 @@ async function all() {
     }
 }
 
-// Update
-function update() {
 
+// Update
+async function updateName(id, name) {
+    const result = await db.result(`
+    update pets set
+        name=$1
+    where id=$2;
+    `, [name, id]);
+if (result.rowCount === 1) {
+    return id;
+} else {
+    return null;
+}
 }
 
-// Delete
-function del() {
 
+
+async function updateBirthdate(id, birthday) {
+    const result = await db.result
+}
+// Delete
+async function del(id) {
+    const result = await db.result(`delete from pets where id=$1`, [id])
+    console.log(result)
+    if (result.rowCount === 1) {
+        return id;
+    } else {
+        return null;
+    }
 }
 
 module.exports = {
     create,
     one,
     all,
-    update,
+    updateName,
     del
 }
