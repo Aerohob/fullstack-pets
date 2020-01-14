c1c9b617bc6049f7e6c088d4543376ddb93d3eed
David Bolton <smdeox@live.com>
2020-01-14 09:58:21 -0500

    Adding routes

--- index.js
+++ index.js
@@ -1,5 +1,62 @@
+const express = require('express')
+const app = express()
+const http = require('http');
 const pets = require('./pets')
+const PORT = 3000;
+// const mustacheExpress = require('mustache-express')
+const es6Renderer = require('es6-template-engine')
+const path = require('path')
+// const VIEWS_PATH = path.join(__dirname, '/views')
+const bodyParser = require('body-parser')
 
+
+// app.engine('mustache', mustacheExpress(VIEWS_PATH + '/partials', '.mustache'))
+app.engine('html', es6Renderer)
+app.set('views', 'templates')
+app.set('view engine', 'html')
+app.use(bodyParser.urlencoded({extended: false}))
+
+
+app.get('/', (req, res) => {
+    res.render('main')
+})
+// Pets 
+app.get('/pets', async (req, res) => {
+    const thePets = await pets.all();
+    res.json(thePets)
+})
+
+app.post('/pets', async (req, res) => {
+    const thePets = await pets.all();
+    res.json(thePets)
+})
+
+// Create
+app.get('pets/create', ( req, res) => {
+    res.render('pets/form')
+})
+app.post('pets/create', bodyParser, async (req, res) => {
+    const { name, species, birthdate } = req.body;
+    // I could create a new pet
+    const owner_id_CHANGEME = 1;
+    await pets.create(name, species, birthdate, owner_id_CHANGEME)
+
+});
+
+// Update
+app.get('/pets/:id/edit')
+app.post('/pets/:id/edit')
+
+// Delete
+app.get('/pets/:id/delete')
+app.post('/pets/:id/delete')
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`)
+})
 // pets.all() 
 // .then(allthePets => {
 
@@ -41,8 +98,10 @@ async function main() {
 }
 main()
 */
+/*
 async function main() {
     const createResult = await pets.create('ITWORKS', 'dog', '2004-04-20', 1);
     console.log(createResult)
 }
 main()
+*/
